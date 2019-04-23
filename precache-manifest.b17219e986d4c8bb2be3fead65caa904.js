52af288941f96807e59202cac3c5ace3495256b0
asmdhabibullah <asmdhabibullah@yahoo.com>
2019-04-23 17:53:03 +0800

    Updates

---
+++ precache-manifest.b17219e986d4c8bb2be3fead65caa904.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e793eaf795e57457ea1b2c2afb8b81c9",
+    "url": "/profile/static/media/preloader.e793eaf7.gif"
+  },
+  {
+    "revision": "739f15e8f78421cd8c94f304643a378a",
+    "url": "/profile/static/media/logo.739f15e8.png"
+  },
+  {
+    "revision": "2af6a9fa4603e8087f20258a1bf6267f",
+    "url": "/profile/static/media/author.2af6a9fa.jpg"
+  },
+  {
+    "revision": "4fba1a2fa32e33d0605c",
+    "url": "/profile/static/js/runtime~main.b2366168.js"
+  },
+  {
+    "revision": "fd229a5470a667b59ada",
+    "url": "/profile/static/js/main.12c08c3f.chunk.js"
+  },
+  {
+    "revision": "131075cf34ff73f28ea1",
+    "url": "/profile/static/js/2.2983ac51.chunk.js"
+  },
+  {
+    "revision": "fd229a5470a667b59ada",
+    "url": "/profile/static/css/main.eff2c561.chunk.css"
+  },
+  {
+    "revision": "3337350e4ddd981c670a8f4cf3cabefe",
+    "url": "/profile/index.html"
+  }
+];
